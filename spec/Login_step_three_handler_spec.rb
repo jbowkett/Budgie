042d0f85b514a2bb5cd0da8dd43c739bdcba41a4
jbowkett <james_bowkett@lyagushka.co.uk>
2013-10-08 18:02:32 +0100

    moved and renamed fixture to allow for other fixtures for other types of prompt

--- spec/Login_step_three_handler_spec.rb
+++ spec/Login_step_three_handler_spec.rb
@@ -9,19 +9,15 @@ describe '#handle' do
     Capybara.run_server = false
     @session = Capybara::Session.new(:selenium)
     # todo: get the current directory out of rspec
-    @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/login_three.html")
   end
 
   let(:memorable_name) { 'memorable name' }
   let(:login_details) { double(:login_details, :memorable_name => memorable_name ) }
 
-  it 'should fill out the second digit in the security code' do
+  it 'should fill out the memorable name' do
+    @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/login_three_memorable_name.html")
     LoginStepThreeHandler.new(login_details).handle(@session)
     @session.find('#memorablename')['value'].should == memorable_name
   end
 
-  xit 'should fill out the third digit in the security code' do
-    LoginStepThreeHandler.new(login_details).handle(@session)
-    @session.find('#secondPassCodeDigit')['value'].should == security_code[2]
-  end
 end
