1e06a6faf9fe9cd3d9b313d1e6ba25033dd097e3
jbowkett <james_bowkett@lyagushka.co.uk>
2013-10-09 18:02:36 +0100

    memorable date input spec

--- spec/Login_step_three_handler_spec.rb
+++ spec/Login_step_three_handler_spec.rb
@@ -12,11 +12,13 @@ describe '#handle' do
   end
 
   let(:memorable_name) { 'memorable name' }
+  let(:memorable_date) { Date.today }
   let(:last_school) { 'Brighton Borstal' }
   let(:first_school) { 'Brighton Borstal' }
   let(:login_details) { double(:login_details, { :memorable_name => memorable_name,
                                                  :last_school => last_school,
-                                                 :first_school => first_school } ) }
+                                                 :first_school => first_school,
+                                                 :memorable_date => memorable_date } ) }
 
   it 'should fill out the memorable name' do
     @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/login_three_memorable_name.html")
@@ -36,4 +38,19 @@ describe '#handle' do
     @session.find('#firstschool')['value'].should == first_school
   end
 
+  context 'for a memorable date' do
+    before :each do
+      @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/login_three_memorable_date.html")
+      LoginStepThreeHandler.new(login_details).handle(@session)
+    end
+    it 'enters the day' do
+      @session.find('#memorabledate')['value'].should == memorable_date.day.to_s
+    end
+    it 'enters the month' do
+      @session.find('#memorabledate').parent.find(:xpath, "//input[@name='memorableMonth']")['value'].should == memorable_date.month.to_s
+    end
+    it 'enters the year' do
+      @session.find('#memorabledate').parent.find(:xpath, "//input[@name='memorableYear']")['value'].should == memorable_date.year.to_s
+    end
+  end
 end
