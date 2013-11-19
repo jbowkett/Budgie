d2b252cbc820b5e3b220c450c82a1c172db2d11c
jbowkett <james_bowkett@lyagushka.co.uk>
2013-11-19 08:35:51 +0000

    corrected following manual test

--- lib/statement_history_handler.rb
+++ lib/statement_history_handler.rb
@@ -1,12 +1,12 @@
 class StatementHistoryHandler
 
   def move_on(page)
-    link_to_most_recent_statement(page).click
+    page.click_link link_to_most_recent_statement(page).text
   end
 
   def link_to_most_recent_statement(page)
     table = page.all('table.summarytable tbody tr')
     first_table_cell = table.first.all('td').first
-    first_table_cell.all('a')
+    first_table_cell.all('a').first
   end
 end
