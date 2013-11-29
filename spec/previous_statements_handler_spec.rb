9dee921c42eb3e15eeaeffaff0e041d5b2accd1a
jbowkett <james_bowkett@lyagushka.co.uk>
2013-11-29 08:10:21 +0000

    added test for credit card previous statements

--- spec/previous_statements_handler_spec.rb
+++ spec/previous_statements_handler_spec.rb
@@ -9,14 +9,29 @@ describe '#extract_transactions' do
     Capybara.javascript_driver = :webkit
     Capybara.run_server = false
     @session = Capybara::Session.new(:selenium)
-    # todo: get the current directory out of rspec
-    @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/statement_back_a_page.html")
     DataMapper.finalize
   end
+  context 'for a regular account' do
+    let (:account){ double(:account, :is_credit_card? => false).as_null_object }
+    before do
+      @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/statement_back_a_page.html")
+    end
 
-  let (:account){ double(:account).as_null_object }
-  it 'extracts the transactions' do
-    previous_txns = PreviousStatementsHandler.new(TransactionExtractor.new(account)).extract_transactions(@session)
-    previous_txns.size.should == 21
+    it 'extracts the transactions' do
+      previous_txns = PreviousStatementsHandler.new(TransactionExtractor.new(account)).extract_transactions(@session)
+      previous_txns.size.should == 21
+    end
+  end
+
+  context 'for a credit card' do
+    let (:account){ double(:account, :is_credit_card? => true).as_null_object }
+    before do
+      @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/statement_back_a_page_credit_card.html")
+    end
+
+    it 'extracts the transactions' do
+      previous_txns = PreviousStatementsHandler.new(TransactionExtractor.new(account)).extract_transactions(@session)
+      previous_txns.size.should == 8
+    end
   end
 end
