d7593655003b5513a923b609dd05086548a29f5b
jbowkett <james_bowkett@lyagushka.co.uk>
2013-11-29 08:43:15 +0000

    file rename

--- spec/Recent_Items_handler_spec.rb
+++ spec/Recent_Items_handler_spec.rb
@@ -35,18 +35,4 @@ describe '#handle' do
       recent_items.size.should == 4
     end
   end
-
-  context 'for a credit card account' do
-    before do
-      @session.visit("file:///Users/jbowkett/other/Smile-Bank-Txn-Downloader/spec/fixtures/recent_items_credit_card.html")
-    end
-
-    let(:account_id) { '1234567890876567' }
-    let(:sort_code)  { nil }
-    let(:is_credit_card) { true }
-    it 'should extract the recent transactions' do
-      recent_items = RecentItemsHandler.new(TransactionExtractor.new(account)).handle(@session, -46863, DateTime.parse('01-01-2000 11:58'))
-      recent_items.size.should == 2
-    end
-  end
 end
